70531e9dc182b4e52b180d9859d9beec2a4236e4
VenterArmand <VenterArmand@users.noreply.github.com>
2017-09-19 15:41:50 +0200

    Add files via upload

---
+++ CheckUserAccountExpiry/CheckUserAccountExpiry/CheckUserAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Movation.Frameworks.Mewh;
+using Movation.Core;
+using Movation.Data;
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+namespace CheckUserAccountExpiry
+{
+    [MewhServiceContract]
+    public class UserAccountExpiry : MewhService
+    {
+        #region Properties
+            [MewhPropertyContract]
+            public string ConnectionString { get; set; }
+        #endregion
+        #region SERVICE METHODS
+            [MewhMethodContract]
+            public void ValidateUserAccount()
+            {
+              try
+              {
+                TSql.ExecuteNonQuery("spCheckUserAccountExpiry", this.ConnectionString);
+              }
+              catch(Exception ex)
+              {
+                EventLogger.LogEvent(EventLogType.Error, ex);
+              }
+            }
+        #endregion
+    }
+}
